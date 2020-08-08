49d0953ba67aac8febd8624de130dbb6e57d3d91
vuhaibang <bangvh@vnfintech.com>
2020-08-08 17:07:22 +0700

    ok

--- service/reup_videos/app.py
+++ service/reup_videos/app.py
@@ -1,4 +1,5 @@
 import moviepy.editor as mpe
+import os
 
 
 def add_audio_in_videos(path_video, path_audio, path_output):
@@ -8,3 +9,13 @@ def add_audio_in_videos(path_video, path_audio, path_output):
     final_audio.write_videofile(path_output)
 
 
+def generator_videos_from_images(dir_path):
+    pass
+
+
+dir_path = '../../images/brightside.me/6 Gym Exercise Alternatives You Can Do at Home'
+list_images = sorted(os.listdir(dir_path))
+list_images = [os.path.join(dir_path, l) for l in list_images]
+clips = [mpe.ImageClip(m).set_duration(3) for m in list_images]
+concat_clip = mpe.concatenate_videoclips(clips, method="compose")
+concat_clip.write_videofile("test.mp4", fps=30)
