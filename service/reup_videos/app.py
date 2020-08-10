c2d6b7e01d4dad9454804bde47fe9cbd668178f0
bang vu hai <bangvh@vnfintech.com>
2020-08-11 01:35:43 +0700

    ok

--- service/reup_videos/app.py
+++ service/reup_videos/app.py
@@ -150,7 +150,15 @@ def gen_video_from_url_image(url_deses, title_video):
     list_audio = [os.path.join(audio_path, l) for l in list_audio]
     path_audio = random.choice(list_audio)
     # audio = mpe.AudioFileClip(path_audio).set_duration(concat_clip.duration)
-    audio = mpe.AudioFileClip(path_audio).set_duration(50)
+    audio = mpe.AudioFileClip(path_audio)
+    if concat_clip.duration < audio.duration:
+        audio = audio.set_duration(concat_clip.duration)
+    else:
+        audios = []
+        for x in range(int(concat_clip.duration/audio.duration)):
+            audios.append(audio)
+        audio = mpe.concatenate_audioclips(audios).set_duration(concat_clip.duration)
+
     concat_clip = concat_clip.set_audio(audio)
     concat_clip.write_videofile(path_video_out_put, fps=15, codec='libx264', threads=4)
 
