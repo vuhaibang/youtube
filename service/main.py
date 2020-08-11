7bee26dcf463d1c934adf89b5ff0a3a3af383f57
vuhaibang <bangvh@vnfintech.com>
2020-08-11 13:50:59 +0700

    ok

--- service/main.py
+++ service/main.py
@@ -13,12 +13,18 @@ links_df = pd.read_csv("brightside_url.csv")['link']
 
 
 t = time.time()
-for link in links_df:
+
+def gen_video(link):
     print(link)
     try:
         title, url_des = get_all_url_image_site_brightside(link)
-    except:
-        continue
-    print(len(url_des))
-    gen_video_from_url_image(url_des, title)
+        print(len(url_des))
+        gen_video_from_url_image(url_des, title)
+    except Exception as e:
+        print(f"Loi {e}")
+
+
+for link in links_df:
+    gen_video(link)
+
 print(f"Tong time {time.time() - t} s")
--- service/reup_videos/app.py
+++ service/reup_videos/app.py
@@ -34,7 +34,7 @@ def add_audio_in_videos(path_video, path_audio, path_output):
     my_clip = mpe.VideoFileClip(path_video)
     audio_background = mpe.AudioFileClip(path_audio)
     final_audio = my_clip.set_audio(audio_background)
-    final_audio.write_videofile(path_output)
+    final_audio.write_videofile(path_output, fps=30, codec='libx264')
 
 
 def replace_space_in_text(text):
@@ -106,13 +106,21 @@ def gen_video():
     path_audio = random.choice(list_audio)
     audio = mpe.AudioFileClip(path_audio).set_duration(concat_clip.duration)
     concat_clip = concat_clip.set_audio(audio)
-    concat_clip.write_videofile("test.mp4", fps=15, codec='libx264', threads=4)
+    concat_clip.write_videofile("test.mp4", fps=30, codec='libx264', threads=4)
 
 def gen_video_from_url_image(url_deses, title_video):
-    path_video_out_put = f'/home/vuhaibangtk/youtube/videos/output/{title_video}.mp4'
-    screensize = (1920, 1080)
+    if os.path.isdir('/home/vuhaibangtk/'):
+        audio_path = '/home/vuhaibangtk/youtube/audio'
+        path_video_out_put = f'/home/vuhaibangtk/youtube/videos/output/{title_video}.mp4'
+        path_intro = f'/home/vuhaibangtk/youtube/videos/intro/fun_pic.mp4'
+        bottom_path = "/home/vuhaibangtk/youtube/service/reup_videos/bottom.png"
+    elif os.path.isdir('/home/vhb/'):
+        audio_path = '/home/vhb/PycharmProjects/Project/youtube/audio'
+        path_video_out_put = f'/home/vhb/PycharmProjects/Project/youtube/videos/output/{title_video}.mp4'
+        path_intro = '/home/vhb/PycharmProjects/Project/youtube/videos/intro/fun_pic.mp4'
+        bottom_path = "/home/vhb/PycharmProjects/Project/youtube/service/reup_videos/bottom.png"
 
-    path_intro = f'/home/vuhaibangtk/youtube/videos/intro/fun_pic.mp4'
+    screensize = (1920, 1080)
     clips = []
     for url, des in url_deses:
         size_text = 80
@@ -125,7 +133,7 @@ def gen_video_from_url_image(url_deses, title_video):
         back_ground_image = mpe.ImageClip(url).resize(screensize).set_duration(duration).set_opacity(0.5)
         txt_mask = mpe.TextClip(text, font='Amiri-Bold', color='black', fontsize=size_text)
         txt_mask = txt_mask.set_duration(duration).set_position("bottom", "center")
-        bottom_image = mpe.ImageClip("/home/vuhaibangtk/youtube/service/reup_videos/bottom.png").resize((1920, line_text * size_text + 20)).set_duration(
+        bottom_image = mpe.ImageClip(bottom_path).resize((1920, line_text * size_text + 20)).set_duration(
             duration).set_position(("center", "bottom"))
         main_image = main_image.resize(
             resize_center_image_in_video(main_image.w, main_image.h, 1920, 1080 - bottom_image.h))\
@@ -145,11 +153,9 @@ def gen_video_from_url_image(url_deses, title_video):
 
 
     concat_clip = mpe.concatenate_videoclips(slided_clips, method="compose").resize(screensize)
-    audio_path = '/home/vuhaibangtk/youtube/audio'
     list_audio = os.listdir(audio_path)
     list_audio = [os.path.join(audio_path, l) for l in list_audio]
     path_audio = random.choice(list_audio)
-    # audio = mpe.AudioFileClip(path_audio).set_duration(concat_clip.duration)
     audio = mpe.AudioFileClip(path_audio)
     if concat_clip.duration < audio.duration:
         audio = audio.set_duration(concat_clip.duration)
@@ -160,7 +166,7 @@ def gen_video_from_url_image(url_deses, title_video):
         audio = mpe.concatenate_audioclips(audios).set_duration(concat_clip.duration)
 
     concat_clip = concat_clip.set_audio(audio)
-    concat_clip.write_videofile(path_video_out_put, fps=15, codec='libx264', threads=4)
-
+    concat_clip.write_videofile(path_video_out_put, fps=30, codec='libx264')
 if __name__ == '__main__':
     conn_brightside = sqlite3.connect('./../../database/brightside.db')
+    add_audio_in_videos("../../videos/videos/St319/1.mp4", "../../videos/audio/St319/1.mp4", "test.mp4")
