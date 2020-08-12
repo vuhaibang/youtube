49f54061819a7dcf0806e4ab06663a4383440616
bang vu hai <bangvh@vnfintech.com>
2020-08-13 00:56:05 +0700

    ok

--- service/reup_videos/app.py
+++ service/reup_videos/app.py
@@ -122,9 +122,11 @@ def gen_video_from_url_image(url_deses, title_video, screensize = (1920, 1080)):
     path_audio = random.choice(list_audio)
     audio = mpe.AudioFileClip(path_audio)
     audio = mpe.afx.audio_loop(audio, duration=concat_clip.duration)
-    concat_clip = concat_clip.set_audio(audio).subclip(100,120)
+    audio = mpe.CompositeAudioClip([mpe.VideoFileClip(path_intro).audio,
+                            audio.set_start(mpe.VideoFileClip(path_intro).duration)])
+    concat_clip = concat_clip.set_audio(audio).subclip(1, 10)
 
-    concat_clip.write_videofile(path_video_out_put, fps=24, codec='libx264', threads=2)
+    concat_clip.write_videofile(path_video_out_put, fps=1, codec='libx264')
 if __name__ == '__main__':
     conn_brightside = sqlite3.connect('./../../database/brightside.db')
     add_audio_in_videos("../../videos/videos/St319/1.mp4", "../../videos/audio/St319/1.mp4", "test.mp4")
