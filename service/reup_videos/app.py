5fd910cf27a8b7f67f83ee485cb0ea35174f7479
vuhaibang <bangvh@vnfintech.com>
2020-08-12 16:59:08 +0700

    ok

--- service/reup_videos/app.py
+++ service/reup_videos/app.py
@@ -120,9 +120,9 @@ def gen_video_from_url_image(url_deses, title_video, screensize = (1920, 1080)):
             audios.append(audio)
         audio = mpe.concatenate_audioclips(audios).set_duration(concat_clip.duration)
 
-    concat_clip = concat_clip.set_audio(audio).subclip(9, 12)
+    concat_clip = concat_clip.set_audio(audio)
 
-    concat_clip.write_videofile(path_video_out_put, fps=1, codec='libx264')
+    concat_clip.write_videofile(path_video_out_put, fps=24, codec='libx264')
 if __name__ == '__main__':
     conn_brightside = sqlite3.connect('./../../database/brightside.db')
     add_audio_in_videos("../../videos/videos/St319/1.mp4", "../../videos/audio/St319/1.mp4", "test.mp4")
